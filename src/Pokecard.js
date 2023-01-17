36377d965a75bfb8d2b49f86ed8a8decaa7d4c98
Daniel Zeljko <danielzeljko@users.noreply.github.com>
2023-01-16 16:07:44 -0800

    add some styling

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,8 +1,10 @@
+import "./Pokecard.css"
+
 const BASE_IMG = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/`
 
 function Pokecard({id, name, type, exp}){
   return (
-    <div id={id}>
+    <div className="Pokecard">
       <p className="Pokecard-name">{name}</p>
       <img className="Pokemon-img" src={`${BASE_IMG}${id}.png`} alt={name}/>
       <p className="Pokecard-type">type: {type}</p>
--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,23 +1,25 @@
-import Pokecard from "./Pokecard"
+import "./Pokedex.css";
+import Pokecard from "./Pokecard";
 
-function Pokedex({pokeList}) {
+function Pokedex({ pokeList }) {
 
   const list = pokeList.map(p => {
     return (
       <Pokecard
+        key={p.id}
         id={p.id}
         name={p.name}
         type={p.type}
         exp={p.base_experience}
       />
-    )
-  })
+    );
+  });
 
-  return(
-    <div>
+  return (
+    <div class="Pokedex">
       {list}
     </div>
-  )
+  );
 }
 
-export default Pokedex
+export default Pokedex;
